70068e6b35a0bf5618abb2683bec18cc275544d3
KrisztinaAnna <krisztina.baranyai@gmail.com>
2016-07-20 18:01:23 +0200

    add connection functions

--- connection.py
+++ connection.py
@@ -1,9 +1,42 @@
 from peewee import *
+import os
 
-def set_db():
+
+def check_dsn_txt():
+    return os.path.isfile('./login.txt')
+
+
+def dsn_input():
     dbname = input("Database name: ")
     username = input("User name: ")
-    db = PostgresqlDatabase(dbname, user=username)
-    return db
+    dsn_list = [dbname, username]
+    with open('login.txt', 'w') as myfile:
+        for word in dsn_list:
+            myfile.write(word + '\n')
+        print("Login file has been created.")
+
+
+def db_data():
+    with open("login.txt", "r") as myfile:
+        lines = myfile.readlines()
+        dsn_list = [line.strip("\n") for line in lines]
+        dbname, username = dsn_list
+        db = PostgresqlDatabase(dbname, user=username)
+        return db
+
+
+def set_db():
+    if check_dsn_txt():
+        answer = input("Login data already exists. Do you want to reenter login data? (Y/n) ")
+        if answer.lower() == "y":
+            dsn_input()
+            return db_data()
+        elif answer.lower() == 'n':
+            return db_data()
+        else:
+            print("Not a valid answer.")
+    else:
+        dsn_input()
+        return db_data()
 
 db = set_db()
