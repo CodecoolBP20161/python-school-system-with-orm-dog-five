4a2b91432526ed1a2c36e7a5ec1667a0d4e23b59
adambdnr <adambodnar@Adams-MacBook-Air.local>
2016-07-21 13:49:11 +0200

    closest school finds itself

--- new_applicants.py
+++ new_applicants.py
@@ -2,3 +2,14 @@
 # You can run it anytime to generate new data!
 
 from models import *
+
+def get_closest_school():
+    query = Closest.select(Closest.school_cid).where(Applicant.home_cid == Closest.home_cid)
+    for applicant in Applicant.select():
+        if applicant.school_cid is None:
+            applicant.school_cid = query
+            applicant.save()
+        else:
+            continue
+
+# get_closest_school()
