1367de1014d69608f88648477f35087c19274252
Komlancz Janos <jkomlancz@gmail.com>
2016-07-21 18:06:13 +0200

    Applicant interview ids are done

--- schedule_interview.py
+++ schedule_interview.py
@@ -1,15 +1,6 @@
 from models import *
 
 
-def check_has_interview():
-    query = Interview.select(Interview.iid)
-    for i in Applicant.select():
-        if i.interview == None:
-            i.interview = query
-            i.save()
-            print(i.interview)
-
-
 def set_reserved():
     reserved_list = []
     for i in Applicant.select().where(Applicant.interview != None):
@@ -22,20 +13,13 @@ def set_reserved():
 
 
 def reserved_interview_to_applicant():
-    none_interview = []
-    for i in Interview.select().where(Interview.reserved != True):
-        none_interview.append(i.iid)
-    for k in none_interview:
-        for applicant in Applicant.select().where(Applicant.interview == None):
-            applicant.interview = k
-            applicant.save()
-            print("OK")
-            continue
-        else:
-            continue
+    none_interview = list(Interview.select().where(Interview.reserved != True))
+    i = len(none_interview)-1
+    for applicant in Applicant.select().where(Applicant.interview == None):
+        applicant.interview = none_interview[i]
+        i -= 1
+        applicant.save()
+
     print(none_interview)
-
-
-#check_has_interview()
 set_reserved()
 reserved_interview_to_applicant()
