03d5458ff3d51a584f3b07e6cfc02d3c51fa052f
robotkutya <mihalyfuredi@gmail.com>
2016-07-21 14:09:42 +0200

    Find new students and assign unique codes and closest cities

--- new_applicants.py
+++ new_applicants.py
@@ -2,6 +2,8 @@
 # You can run it anytime to generate new data!
 
 from models import *
+from random import randint
+
 
 def get_closest_school():
     query = Closest.select(Closest.school_cid).where(Applicant.home_cid == Closest.home_cid)
@@ -12,4 +14,28 @@ def get_closest_school():
         else:
             continue
 
-# get_closest_school()
+
+def read_codes(Applicant):
+    code_set = set()
+    for appl_record in Applicant.select():
+        code_set.add(appl_record.application_code)
+    try:
+        code_set.remove(None)
+    except KeyError:
+        pass
+    return(code_set)
+
+
+def update_appl_code(Applicant):
+    code_set = read_codes(Applicant)
+    for appl_record in Applicant.select():
+        while True:
+            if appl_record.application_code in code_set:
+                break
+            appl_record.application_code = randint(100, 999)
+        code_set.add(appl_record.application_code)
+        appl_record.save()
+
+
+get_closest_school()
+update_appl_code(Applicant)
