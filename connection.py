9f4ed033936ed930cdd84ede2f6ddb16ae5920ed
KrisztinaAnna <krisztina.baranyai@gmail.com>
2016-07-22 12:33:47 +0200

    change connection.py to class

--- connection.py
+++ connection.py
@@ -1,47 +1,48 @@
 from peewee import *
 import os
 
+class Connection:
+    '''Handles everything related to database connection'''
 
-def check_dsn_txt():
-    return os.path.isfile('./login.txt')
+    # check whether the login file already exits
+    @staticmethod
+    def check_dsn_txt():
+        return os.path.isfile('./login.txt')
 
+    # saves PostgreSQL login data into txt file
+    @staticmethod
+    def dsn_input():
+        dbname = input("Database name: ")
+        username = input("User name: ")
+        dsn_list = [dbname, username]
+        with open('login.txt', 'w') as myfile:
+            for word in dsn_list:
+                myfile.write(word + '\n')
+            print("Login file has been created.")
 
-def dsn_input():
-    dbname = input("Database name: ")
-    username = input("User name: ")
-    dsn_list = [dbname, username]
-    with open('login.txt', 'w') as myfile:
-        for word in dsn_list:
-            myfile.write(word + '\n')
-        print("Login file has been created.")
+    @classmethod
+    # reads postgresql login data from txt file and defines db
+    def db_data(cls):
+        with open("login.txt", "r") as myfile:
+            lines = myfile.readlines()
+            dsn_list = [line.strip("\n") for line in lines]
+            dbname, username = dsn_list
+            cls.db = PostgresqlDatabase(dbname, user=username)
+            return cls.db
 
+    # checks whether the login file already exists and creates connection with PostgreSQL
+    @classmethod
+    def set_db(cls):
+        if cls.check_dsn_txt():
+            return cls.db_data()
+        else:
+            cls.dsn_input()
+            return cls.db_data()
 
-def db_data():
-    with open("login.txt", "r") as myfile:
-        lines = myfile.readlines()
-        dsn_list = [line.strip("\n") for line in lines]
-        dbname, username = dsn_list
-        db = PostgresqlDatabase(dbname, user=username)
-        return db
-
-
-def set_db():
-    if check_dsn_txt():
-        return db_data()
-    else:
-        dsn_input()
-        return db_data()
-
-
-# def error_db():
-#     answer = input("Error in login data. Do you want to reenter login data? (Y/n) ")
-#     if answer.lower() == "y":
-#         dsn_input()
-#         return db_data()
-#     elif answer.lower() == 'n':
-#         break
-#     else:
-#         print("Not a valid answer.")
-#         break
-
-db = set_db()
+    # if there is an error with connecting to PostgreSQL this function provides the option to reenter
+    # login data
+    @classmethod
+    def error_db(cls):
+        answer = print('Cannot connect to PostgreSQL. Check whether PostgrSQL is running and reenter login data!')
+        cls.dsn_input()
+        cls.set_db()
