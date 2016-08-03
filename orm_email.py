da6985917a9020d0ddf3f7331219f840479aea2f
KrisztinaAnna <krisztina.baranyai@gmail.com>
2016-08-03 15:14:12 +0200

    add send function to OrmEmail class

--- orm_email.py
+++ orm_email.py
@@ -2,19 +2,33 @@ from applicant import Applicant
 from mentor import Mentor
 from connection import Connection
 from smtplib import *
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
-class email(Connection):
+class OrmEmail(Connection):
     """creates messages and send e-mails"""
 
     # Connection has class attribute server, etc
     # msg and login comes from applicant and here
     @classmethod
-    def send(cls, msg, login):
+    def send(cls):
         # receive msg object, login info and send email via smtplib method
-        pass
+        msg = cls.create_newapp()
+        server, fromaddr, password = Connection.set_smtp()
+        server.starttls()
+        server.login(fromaddr, password)
+        server.send_message(msg)
+        server.quit()
 
-    @staticmethod
-    def create_newapp():
-        # use applicant method to get info then make msg object
-        pass
+
+    @classmethod
+    def create_newapp(cls):
+        smtp_stuff = cls.create_newapp()
+        msg = MIMEText('helloooo')
+        msg['Subject'] = 'targy'
+        msg['From'] = 'dog5.laboratories@gmail.com'
+        msg['To'] = 'dog5.laboratories@gmail.com'
+        return msg
+
+OrmEmail.send()
