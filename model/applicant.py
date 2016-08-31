1c4b063b4576ea7977e88717bcc980ac383ae7cc
robotkutya <mihalyfuredi@gmail.com>
2016-08-31 14:21:54 +0200

    save record in applicant table

--- model/applicant.py
+++ model/applicant.py
@@ -2,6 +2,7 @@ from peewee import *
 from model.basemodel import BaseModel
 from model.city import City
 from model.school import School
+from random import randint
 
 
 class Applicant(BaseModel):
@@ -15,3 +16,55 @@ class Applicant(BaseModel):
     sent_application_email = BooleanField(default=False)
     sent_interview_email = BooleanField(default=False)
     code_set = set()
+
+
+    # assigns closest school for applicant
+    @classmethod
+    def get_closest_school(cls):
+        query = Closest.select(Closest.school).where(cls.home == Closest.home)
+        for applicant in cls.select():
+            if applicant.school is None:
+                applicant.school = query
+                applicant.save()
+            else:
+                continue
+
+
+    # collects appl codes into a set
+    @classmethod
+    def read_codes(cls):
+        code_set = set()
+        for appl_record in cls.select():
+            code_set.add(appl_record.application_code)
+        return(code_set)
+
+
+    # assigns closest school for applicant
+    @classmethod
+    def update_appl_code(cls):
+        cls.code_set = cls.read_codes()
+        for appl in cls.select().where(cls.application_code == None):
+            appl.generate_code()
+            appl.save()
+
+
+    # generates unique code for applicant record
+    def generate_code(self):
+        while True:
+            if self.application_code not in self.code_set:
+                break
+            self.application_code = randint(100, 999)
+        self.code_set.add(self.application_code)
+
+
+    # saves applicant as record from dict input
+    @classmethod
+    def add_applicant(cls, reg_dict):
+        applicant = cls.create(name=reg_dict['name'],
+                               home=reg_dict['home'],
+                               email=reg_dict['email'])
+
+
+    # checks user input in dictionary and returns with error
+    def check_record():
+        pass
