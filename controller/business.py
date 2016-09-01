1236e441f9e6657961b7e656115832ebc597e230
robotkutya <mihalyfuredi@gmail.com>
2016-09-01 09:37:54 +0200

    new applicant validation logic

--- controller/business.py
+++ controller/business.py
@@ -2,7 +2,6 @@ from model.applicant import Applicant
 
 
 # This function manages the submit function from the /registration route
-def validate(reg_dict):
-    Applicant.add_applicant(reg_dict)
-    reg_dict_with_error = reg_dict
+def register_applicant(reg_dict):
+    reg_dict_with_error = Applicant.validate(reg_dict)
     return(reg_dict_with_error)
--- model/applicant.py
+++ model/applicant.py
@@ -9,14 +9,14 @@ class Applicant(BaseModel):
     """Applicant model/table and data manipulation related to it"""
     name = CharField()
     application_code = IntegerField(null=True, unique=True)
-    home = ForeignKeyField(City, related_name="appl_home")
+    home = ForeignKeyField(City, related_name="appl_home", null=True)
     school = ForeignKeyField(School, related_name="appl_school", null=True)
     # in real life this should be unique, but we send all e-mails to the same e-mail account in our test data
     email = CharField()
     sent_application_email = BooleanField(default=False)
     sent_interview_email = BooleanField(default=False)
     code_set = set()
-
+    registration_error_set = set()
 
     # assigns closest school for applicant
     @classmethod
@@ -29,7 +29,6 @@ class Applicant(BaseModel):
             else:
                 continue
 
-
     # collects appl codes into a set
     @classmethod
     def read_codes(cls):
@@ -37,7 +36,6 @@ class Applicant(BaseModel):
         for appl_record in cls.select():
             code_set.add(appl_record.application_code)
         return(code_set)
-
 
     # assigns closest school for applicant
     @classmethod
@@ -47,7 +45,6 @@ class Applicant(BaseModel):
             appl.generate_code()
             appl.save()
 
-
     # generates unique code for applicant record
     def generate_code(self):
         while True:
@@ -56,15 +53,35 @@ class Applicant(BaseModel):
             self.application_code = randint(100, 999)
         self.code_set.add(self.application_code)
 
-
-    # saves applicant as record from dict input
     @classmethod
-    def add_applicant(cls, reg_dict):
-        applicant = cls.create(name=reg_dict['name'],
-                               home=reg_dict['home'],
-                               email=reg_dict['email'])
+    def validate(cls, reg_dict):
+        try:
+            home_city = City.get(name=reg_dict['home'])
+            home_id = home_city.id
+        except City.DoesNotExist:
+            home_id = None
+        applicant = cls(name=reg_dict['name'],
+                        home=home_id,
+                        email=reg_dict['email'])
+        applicant.check_name()
+        applicant.check_home()
+        applicant.check_email()
+        if applicant.registration_error_set == set():
+            applicant.save()
+            return None
+        else:
+            reg_dict_with_error = reg_dict
+            reg_dict_with_error['error'] = applicant.registration_error_set
+            return(reg_dict_with_error)
 
+    def check_name(self):
+        if ' ' not in self.name:
+            self.registration_error_set.add('name')
 
-    # checks user input in dictionary and returns with error
-    def check_record():
-        pass
+    def check_home(self):
+        if self.home is None:
+            self.registration_error_set.add('home')
+
+    def check_email(self):
+        if '@' or '.' not in self.email:
+            self.registration_error_set.add('email')
